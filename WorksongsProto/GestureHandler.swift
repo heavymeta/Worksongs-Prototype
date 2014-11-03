f2b653ab1552e18f66b14c5ff6f7cfa9385daf81
Ian Curry <ian@heavy-meta.com>
2014-11-03 15:44:50 -0500

    Updated

---
+++ WorksongsProto/GestureHandler.swift
@@ -0,0 +1,20 @@
+//
+//  GestureHandler.swift
+//  WorksongsProto
+//
+//  Created by Ian Curry on 10/18/14.
+//  Copyright (c) 2014 Ian Curry. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GestureHandler: UIViewController {
+    
+    func test() -> Int
+    {
+        var x:Int = 10
+        return x
+    }
+    
+}
--- WorksongsProto/ViewController.swift
+++ WorksongsProto/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var but: UIView!
+    
+    var audioPlayer = AVAudioPlayer()
+    
+    var filename:String!
+    var type:String!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        self.playSound()
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        self.stopSound()
     }
 
+    func playSound()
+    {
+        filename = "join"
+        type = "mp3"
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        var alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(filename, ofType: type)!)
+        println(alertSound)
+        
+        // Removed deprecated use of AVAudioSessionDelegate protocol
+        AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, error: nil)
+        AVAudioSession.sharedInstance().setActive(true, error: nil)
+        
+        var error:NSError?
+        audioPlayer = AVAudioPlayer(contentsOfURL: alertSound, error: &error)
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+    }
+    
+    func stopSound()
+    {
+        audioPlayer.stop()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
